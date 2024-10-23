820752789807b832fa2063e7e97bd6434ecf8132
HACKBOX010 <Budit6388@gmail.com>
2024-10-23 23:35:42 +0530

    first python code

--- DAY1.py
+++ DAY1.py
@@ -1,1 +1,17 @@
 print("welcome to day 1")
+
+a=int(input("enter your choice:"))
+b=int(input("enter your choice:"))
+print("sum:",a+b)
+
+a=int(input("enter your choice:"))
+b=int(input("enter your choice:"))
+print("difference",a-b)
+
+a=int(input("enter your choice:"))
+b=int(input("enter your choice:"))
+print("product",a*b)
+
+a=int(input("enter your choice:"))
+b=int(input("enter your choice:"))
+print("division",a/b)
