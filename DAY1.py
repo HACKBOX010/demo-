a1e027d027d76213e746967a17395f2dcd2ae322
HACKBOX010 <Budit6388@gmail.com>
2024-10-25 19:03:40 +0530

    DAY1

--- DAY1.py
+++ DAY1.py
@@ -1,17 +1,38 @@
-print("welcome to day 1")
+# code to print hello world
+#print("hello world")
 
-a=int(input("enter your choice:"))
-b=int(input("enter your choice:"))
-print("sum:",a+b)
 
-a=int(input("enter your choice:"))
-b=int(input("enter your choice:"))
-print("difference",a-b)
+#basic addition
+#Q - Write a code for addition of two numbers.
+#a=int(input("enter your choice:"))
+#b=int(input("enter your choice:"))
+#print("sum:",a+b)
 
-a=int(input("enter your choice:"))
-b=int(input("enter your choice:"))
-print("product",a*b)
+#basic subtraction
+# Q - write a code for subtraction  two number.
+#a=int(input("enter your choice:"))
+#b=int(input("enter your choice:"))
+#print("difference",a-b)
 
-a=int(input("enter your choice:"))
-b=int(input("enter your choice:"))
-print("division",a/b)
+#basic multiplication
+# Q- Write a code for multiplication of two numbers.
+#a=int(input("enter your choice:"))
+#b=int(input("enter your choice:"))
+#print("product",a*b)
+
+#basic division
+#Q - Write a code for division of two numbers.
+#a=int(input("enter your choice:"))
+#b=int(input("enter your choice:"))
+#print("division",a/b)
+
+# check if its even or odd
+
+def check_even_odd(number):
+    num = int(input("enter a number:"))
+    result = check_even_odd(number)
+    if number % 2 == 0:
+        return"even"
+    else:
+        return "odd"
+ print(result)
