f9995411b086e3bebea37b200f750aa3a82f8771
HACKBOX010 <Budit6388@gmail.com>
2024-10-26 20:00:40 +0530

    slicing in python

--- DAY1.py
+++ DAY1.py
@@ -28,11 +28,14 @@
 
 # check if its even or odd
 
-def check_even_odd(number):
-    if number % 2 == 0:
-        return"even"
-    else:
-        return "odd"
-num = int(input("enter a number:"))
-result = check_even_odd(num)
-print(result)
+# def check_even_odd(number):
+#     if number % 2 == 0:
+#         return"even"
+#     else:
+#         return "odd"
+# num = int(input("enter a number:"))
+# result = check_even_odd(num)
+# print(result)
+
+#Name = ("udit bhandari")
+#print(Name[2:5])
