ed2fc83bed902da6bdfb80d447d8ff156a480182
HACKBOX010 <Budit6388@gmail.com>
2024-10-26 20:18:19 +0530

    SLICING IN PYTHON

--- SLICING IN PYTHON.py
+++ SLICING IN PYTHON.py
@@ -1,26 +1,26 @@
 #Q1 :-xtracting a Substring:
 # Given the string "Hello, World!", extract the substring "World" using slicing.
 #code below :-
-#a = "hello,world"
-#print(a[9:5])
+a = "hello,world"
+print(a[9:5])
 
 #Q2 :- Reversing a String:
 #Write a slice operation to reverse the string "Python".
 #code below :-
-#a="python"
-#print(a[::-1])
+a="python"
+print(a[::-1])
 
 #Q3 :- Skipping Characters:
 #From the string "abcdefgh", extract every second character starting from the first.
 #code below  :-
-#a ="abcdefgh"
-#print(a[::2])
+a ="abcdefgh"
+print(a[::2])
 
 #Q4 :- First and Last Characters:
 #Slice and print the first and last characters from the string "SDET".
 #code below :-
-#a="SDET"
-#print(a[0:-1])
+a="SDET"
+print(a[0:-1])
 
 #Q5 :- Remove First n Characters:
 #Given the string "OpenAI", write a slice operation to remove the first 2 characters.
