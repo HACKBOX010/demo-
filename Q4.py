59301eb981f70dbb006e5ff86507aac67beb8342
HACKBOX010 <Budit6388@gmail.com>
2024-10-29 15:50:34 +0530

    Q4.py

---
+++ Q4.py
@@ -0,0 +1,10 @@
+#Q4 Write a Python program to swap two variables.
+#code below:
+a =int(input("ENTER YOUR CHOICE:"))
+b =int(input("ENTER YOUR CHOICE"))
+temp = a #using temp or temporary variable fucniton
+a = b
+b = temp
+print("after swap")
+print("a:",a)
+print("b:",b)
