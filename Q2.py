b23ee2ff39360db8d503d03d213700b1014db2be
HACKBOX010 <Budit6388@gmail.com>
2024-10-29 15:29:02 +0530

    Q2.py

---
+++ Q2.py
@@ -0,0 +1,16 @@
+#Q2 :- Write a Python program to do arithmetical operations addition and division
+# NOTE :- we can do it by two ways user input or selfgiven values.
+#CODE BELOW :-
+#WAY 1 :- SELFGIVEN VALUES
+a = 100
+b = 20
+print("sum",a+b)
+print("division",a/b)
+
+#WAY 2:- USER INPUT
+a =int(input("ENTER YOUR CHOICE:"))
+b =int(input("ENTER YOUR CHOICE:"))
+print("sum:",a+b)
+print("division:",a/b)
+
+
