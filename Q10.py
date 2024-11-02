d007925596c1f1f0aa6f73f036af0844998caa06
HACKBOX010 <Budit6388@gmail.com>
2024-11-02 22:32:17 +0530

    Q10.py

---
+++ Q10.py
@@ -0,0 +1,12 @@
+#Write a Python program to swap two variables without temp variable.
+#code below :
+
+a =float(input("ENTER YOUR CHOICE FOR a:"))
+b =float(input("ENTER YOUR CHOICE FOR b:"))
+
+#logic for swaping
+a, b= b, a
+#output
+print("After swaping")
+print("a",a)
+print("b",b)
