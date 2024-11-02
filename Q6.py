cce2c36205c09d57ecdd3c0eb3aa3af8722dbbc0
HACKBOX010 <Budit6388@gmail.com>
2024-11-02 22:18:52 +0530

    Q6.py

---
+++ Q6.py
@@ -0,0 +1,7 @@
+# Write a Python program to convert Celsius to Fahrenheit.
+#code below :-
+celsius = float(input("ENTER YOUR CHOICE :"))
+#FORMULA TO BE USED
+fahrenheit =(celsius*9/5)+32
+#output
+print("degree in  fahrenheit:- ",fahrenheit)
